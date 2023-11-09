731aac7424963da0f99838c2381c51beddd050d6
Robert Scoville <robertjamesscoves@gmail.com>
2023-11-09 10:29:22 -0600

    dark mode

---
+++ app.js
@@ -0,0 +1,14 @@
+const darkButton = document.getElementById('dark')
+darkButton.addEventListener('click', darkMode)
+let lights = true
+function darkMode() {
+    if (!lights) {
+        document.body.style.backgroundColor = 'white'
+        document.body.style.color = 'black'
+        lights == true
+    } else {
+        document.body.style.backgroundColor = 'darkgrey'
+        document.body.style.color = 'white'
+        lights == false
+    }
+}
